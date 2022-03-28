939e0fbc4b550b6a8331722eda3c86cc7794deb9
liuxd0911 <lxd0911@gmail.com>
2022-03-28 13:06:04 +0800

    update

--- src/main/java/ubsi/demo/hello/Config.java
+++ src/main/java/ubsi/demo/hello/Config.java
@@ -2,8 +2,8 @@ package ubsi.demo.hello;
 
 /** 微服务的配置 */
 public class Config {
-    public String name;                             // 配置项
-    public String name_restart;                     // 如果配置项修改后必须重启微服务才能生效，则@USConfigGet时通过???_restart返回
-                                                    // 配置项在配置文件中的值（非运行时的参数值）
-    public String name_comment = "服务实例的名字";    // "name"配置项的说明
+    public String name;                          // 配置项的当前运行值
+    public String name_restart;                  // 配置项的配置值（在配置文件中的值，非当前值）
+                                                 // 如果配置项修改后须重启才能生效，则通过???_restart返回
+    public String name_comment = "服务实例的名字"; // 配置项的说明，通过???_comment返回
 }
