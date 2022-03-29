03d73423350e51d7a6ae4d94f5326387e67c5542
liuxd0911 <lxd0911@gmail.com>
2022-03-29 14:55:22 +0800

    update

--- src/main/java/ubsi/demo/hello/Test.java
+++ src/main/java/ubsi/demo/hello/Test.java
@@ -7,39 +7,25 @@ public class Test {
 
     /** 命令行启动入口 */
     public static void main(String[] args) throws Exception {
-        Context.startup(".");   // 启动UBSI Consumer
-        // 访问微服务前必须：UBSI容器已经在localhost:7112上启动，并部署了ubsi.demo.hello微服务
+        Context.startup(".");   // 启动UBSI Consumer，指定"."为当前工作目录
         callAsync();
         callSync();
         Context.shutdown();              // 关闭UBSI Consumer
-
-        /* 命令行输出结果：
-return: hello consumer-sync
-code: 0, data: hello consumer-async
-
-           容器console的输出：
-[INFO]  2022-03-04 16:26:58.233 liuxd-hp#7112   rewin.ubsi.container    rewin.ubsi.container    [1]rewin.ubsi.container.Bootstrap#start()#154   startup      "2.3.0"
-[INFO]  2022-03-04 16:27:34.953 liuxd-hp#7112   rewin.ubsi.service      ubsi.demo.hello [37]ubsi.demo.hello.Service#init()#35   start   "ubsi"
-[INFO]  2022-03-04 16:29:33.923 liuxd-hp#7112   rewin.ubsi.service      ubsi.demo.hello#hello   [51]ubsi.demo.hello.ServiceEntry#hello()#20     consumer-async  "hello ubsi"
-[INFO]  2022-03-04 16:29:33.923 liuxd-hp#7112   rewin.ubsi.service      ubsi.demo.hello#hello   [50]ubsi.demo.hello.ServiceEntry#hello()#20     consumer-sync   "hello ubsi"
-[INFO]  2022-03-04 16:36:32.299 liuxd-hp#7112   rewin.ubsi.service      ubsi.demo.hello [20]ubsi.demo.hello.Service#close()#42  stop    "ubsi"
-[INFO]  2022-03-04 16:36:32.317 liuxd-hp#7112   rewin.ubsi.container    rewin.ubsi.container    [20]rewin.ubsi.container.Bootstrap#stop()#190   shutdown        "2.3.0"
-         */
     }
 
     // 同步方式请求
     static void callSync() throws Exception {
         Context ubsi = Context.request("ubsi.demo.hello", "hello", "consumer-sync"); // 创建UBSI请求对象
-        String ack = (String)ubsi.direct("localhost", 7112);    // 直接向指定容器发送同步请求，并等待返回结果
+        String ack = (String)ubsi.call();    // 同步方式发送请求，并等待返回结果
         System.out.println("return: " + ack);
     }
 
     // 异步方式请求
     static void callAsync() throws Exception {
         Context ubsi = Context.request("ubsi.demo.hello", "hello", "consumer-async"); // 创建UBSI请求对象
-        ubsi.directAsync("localhost", 7112, (code, data) -> {
+        ubsi.callAsync((code, data) -> {
             // 在请求结果通知中的处理动作（如果是高耗时操作，为避免长时间占用I/O线程，应启动单独线程处理）
             System.out.println("code: " + code + ", data: " + data);
-        }, false);   // 直接向指定容器发送异步请求
+        }, false);   // 异步方式发送请求，通过回调得到结果
     }
 }
--- src/test/java/ubsi/demo/hello/ServiceEntryTest.java
+++ src/test/java/ubsi/demo/hello/ServiceEntryTest.java
@@ -21,12 +21,13 @@ public class ServiceEntryTest {
         Bootstrap.start();      // 启动测试容器，缺省为 localhost#7112
         /*
           启动容器时需要rewin.ubsi.module.json文件，否则容器不会自动加载微服务
+          微服务加载时如果"startup"为true，则启动微服务（执行@USInit方法）
          */
     }
 
     @After
     public void after() throws Exception {
-        Bootstrap.stop();       // 关闭测试容器
+        Bootstrap.stop();       // 关闭测试容器，关闭时会依次停止加载的所有微服务（执行@USClose方法）
     }
 
     /**
@@ -34,9 +35,13 @@ public class ServiceEntryTest {
     */
     @Test
     public void testHello() throws Exception {
+        // 创建ubsi请求对象
         Context ubsi = Context.request(Service.SERVICE_NAME, "hello", "tester");
+        // 发起ubsi请求：direct直连方式通常用于测试/容器治理，正常情况下应该使用call()路由方式
         String ack = (String)ubsi.direct("localhost", 7112);
+        // 通过日志输出结果
         Context.getLogger("junit", "test-hello").info("return", ack);
+        // 通过console输出结果
         System.out.println("return: " + ack);
     }
 
@@ -46,10 +51,12 @@ public class ServiceEntryTest {
      */
     @Test
     public void testConfig() throws Exception {
+        // 获取微服务的配置参数：向容器控制器（名为""的微服务）发getConfig请求
         Context ubsi = Context.request("", "getConfig", Service.SERVICE_NAME);
         Map config = (Map)ubsi.direct("localhost", 7112);   // UBSI在传输数据时会将自定义的Java-Class映射为Map
         Request.printJson(config);  // 输出获取到的配置信息
 
+        // 设置微服务的配置参数：向容器控制器（名为""的微服务）发setConfig请求
         Map new_cfg = Util.toMap("name", "new-name");
         String cfg_json = new Gson().toJson(new_cfg);
         ubsi = Context.request("", "setConfig", Service.SERVICE_NAME, cfg_json);
